735e53be3f9b48869cbdafea08be7fc467814c6e
CARLOS ALBERTO DA SILVA JUNIOR <carlos.junior@etec.sp.gov.br>
2025-08-08 11:56:07 -0300

    projetin pronto

---
+++ App.js
@@ -0,0 +1,121 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Alert, Image } from 'react-native';
+
+export default function App() {
+  const handleAcessar = () => {
+    Alert.alert('Você clicou em "Como deseja acessar?"');
+  };
+
+  const handleOutrasOpcoes = () => {
+    Alert.alert('Você clicou em "Outras opções"');
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.topBar}>
+  <Text style={styles.topBarText}></Text>
+</View>
+      <Image 
+        source={require('./assets/casual_dog.png')} 
+        style={styles.image} 
+      />
+      <Text style={styles.title}>Ótimo dia!</Text>
+      <Text style={styles.subtitle}>Como deseja acessar?</Text>
+      <TouchableOpacity style={styles.button} onPress={handleAcessar}>
+        <View style={styles.buttonContent}>
+          <Image 
+            source={require('./assets/Google.png')} 
+            style={styles.buttonImage} 
+          />
+          <Text style={styles.buttonText}>Como deseja acessar?</Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.outlineButton} onPress={handleOutrasOpcoes}>
+  <Text style={styles.outlineButtonText}>Outras opções</Text>
+</TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f0f0f0',
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    color: '#333',
+  },
+  image: {
+    width: 355, // Largura da imagem
+    height: 300, // Altura da imagem
+    marginBottom: 20, 
+    marginTop: 200,
+  },
+  subtitle: {
+    fontSize: 18,
+    marginBottom: 20,
+    color: '#555',
+  },
+  button: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 300,
+    height: 50,
+    backgroundColor: '#2cb859',
+    borderRadius: 5,
+    marginVertical: 10,
+  },
+  buttonContent: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  buttonImage: {
+    backgroundColor: '#fff',
+    width: 24,
+    height: 24, 
+    marginRight: 10, 
+  },
+  outlineButton: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 300,
+    height: 50,
+    backgroundColor: '#fff', 
+    borderWidth: 2, 
+    borderColor: '#2cb859', 
+    borderRadius: 5,
+    marginVertical: 10,
+  },
+  outlineButtonText: {
+    color: '#131a20', 
+    fontSize: 16,
+    textAlign: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    textAlign: 'center',
+  },
+  topBar: {
+    width: '100%',
+    height: 60, // Altura da aba
+    backgroundColor: '#ccc', // Fundo cinza
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    top: 0, // Posiciona no topo
+    zIndex: 1, // Garante que fique acima de outros elementos
+  },
+  topBarText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#333', // Cor do texto
+  },
+});
