ca661d85daf6e16c3e653537203dcacd49bbc5eb
CARLOS ALBERTO DA SILVA JUNIOR <carlos.junior@etec.sp.gov.br>
2025-08-20 11:27:21 -0300

    ajustes finais

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -39,8 +39,85 @@ export default function Home({ navigation }) {
     );
 }
 
-const styles = StyleSheet.create({ container:
-     { flex: 1, justifyContent: 'center',
-         alignItems: 'center', 
-         backgroundColor: '#f0f0f0',
-         }, title: { fontSize: 40, fontWeight: 'bold', marginBottom: 10, color: '#333', }, image: { width: 355, height: 300, marginBottom: 20, marginTop: 200, }, subtitle: { fontSize: 18, marginBottom: 20, color: '#555', }, button: { flexDirection: 'row', alignItems: 'center', justifyContent: 'center', width: 300, height: 50, backgroundColor: '#2cb859', borderRadius: 5, marginVertical: 10, }, buttonContent: { flexDirection: 'row', alignItems: 'center', }, buttonImage: { backgroundColor: '#fff', width: 24, height: 24, marginRight: 10, }, outlineButton: { flexDirection: 'row', alignItems: 'center', justifyContent: 'center', width: 300, height: 50, backgroundColor: '#fff', borderWidth: 2, borderColor: '#2cb859', borderRadius: 5, marginVertical: 10, }, outlineButtonText: { color: '#131a20', fontSize: 16, textAlign: 'center', }, buttonText: { color: '#fff', fontSize: 16, textAlign: 'center', }, topBar: { width: '100%', height: 60, backgroundColor: '#ccc', justifyContent: 'center', alignItems: 'center', position: 'absolute', top: 0, zIndex: 1, }, topBarText: { fontSize: 18, fontWeight: 'bold', }, });
+const styles = StyleSheet.create({ 
+          container: { 
+          flex: 1, 
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: '#f0f0f0',
+        },
+        title: { 
+          fontSize: 40, 
+          fontWeight: 'bold', 
+          marginBottom: 10, 
+          color: '#333', 
+        },
+        image: { 
+          width: 355, 
+          height: 300, 
+          marginBottom: 20, 
+          marginTop: 200, 
+        },
+        subtitle: { 
+          fontSize: 18, 
+          marginBottom: 20, 
+          color: '#555', 
+        },
+        button: { 
+          flexDirection: 'row', 
+          alignItems: 'center', 
+          justifyContent: 'center', 
+          width: 300, 
+          height: 50, 
+          backgroundColor: '#2cb859', 
+          borderRadius: 5, 
+          marginVertical: 10, 
+        },
+        buttonContent: { 
+          flexDirection: 'row', 
+          alignItems: 'center', 
+        },
+        buttonImage: { 
+          backgroundColor: '#fff', 
+          width: 24, 
+          height: 24, 
+          marginRight: 10, 
+        },
+        outlineButton: { 
+          flexDirection: 'row', 
+          alignItems: 'center', 
+          justifyContent: 'center', 
+          width: 300, 
+          height: 50, 
+          backgroundColor: '#fff', 
+          borderWidth: 2, 
+          borderColor: '#2cb859', 
+          borderRadius: 5, 
+          marginVertical: 10, 
+        },
+        outlineButtonText: { 
+          color: '#131a20', 
+          fontSize: 16, 
+          textAlign: 'center', 
+        },
+        buttonText: { 
+          color: '#fff', 
+          fontSize: 16, 
+          textAlign: 'center', 
+        },
+        topBar: { 
+          width: '100%', 
+          height: 60, 
+          backgroundColor: '#ccc', 
+          justifyContent: 'center', 
+          alignItems: 'center', 
+          position: 'absolute', 
+          top: 0, 
+          zIndex: 1, 
+        },
+        topBarText: { 
+          fontSize: 18, 
+          fontWeight: 'bold', 
+        },
+      });
+      
